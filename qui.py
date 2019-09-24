10b177e97934bc2fcbceb8a1cb1b687d7da0a6e3
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-09-24 15:13:35 +0300

    Added qui.py

---
+++ qui.py
@@ -0,0 +1,21 @@
+# 
+# 
+# J.Valtteri 24.9.2019
+# QUI
+
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+#
+qt_app = QApplication(sys.argv)
+
+#
+label = QLabel(G-Tycoon)
+
+#
+label.show()
+
+#
+qt_app.excec_()
+

0154eb1a6f0cd549ef15580d6d6a7d4f6bd827fc
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-09-25 13:58:37 +0300

    init qt stuff

--- qui.py
+++ qui.py
@@ -7,15 +7,28 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
+# from pyside tutorial t13
+
+class LCDRange(QWidget):
+    def __init__(self, text=None, parent=None):
+        if isinstance(text, QWidget):
+            parent = text
+            text = None
+
+        QWidget.__init__(self, parent)
+
+        self.init()
+
+        if text:
+            self.setText(text)
+
+    def init(self):
+        lcd = QLCDNumber(2)
+        self.slider = QSlider( ####
+
 #
 qt_app = QApplication(sys.argv)
-
-#
 label = QLabel(G-Tycoon)
-
-#
 label.show()
-
-#
 qt_app.excec_()
 

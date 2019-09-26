ea3a7cc864f6579e2a8230400b7ddfafbdad1972
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-09-26 14:31:09 +0300

    Filled in the slider example

--- qui.py
+++ qui.py
@@ -1,11 +1,11 @@
-# 
-# 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # J.Valtteri 24.9.2019
 # QUI
 
 import sys
-from PySide.QtCore import *
-from PySide.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
 
 # from pyside tutorial t13
 
@@ -24,11 +24,53 @@ class LCDRange(QWidget):
 
     def init(self):
         lcd = QLCDNumber(2)
-        self.slider = QSlider( ####
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setRange(-24, 24)
+        self.slider.setValue(-13)
+        self.label = QLabel()
+
+        self.label.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        self.label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+
+        self.connect(self.slider, SIGNAL("valueChanged(int)"),
+                     lcd, SLOT("display(int)"))
+        
+        self.connect(self.slider, SIGNAL("valueChanged(int)"),
+                     self, SIGNAL("valueChanged(int)"))
+
+        layout = QVBoxLayout()
+        layout.addWidget(lcd)
+        layout.addWidget(self.slider)
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+        self.setFocusProxy(self.slider)
+
+    def value(self):
+        return self.slider.value()
+
+    def setValue(self, value):
+        self.slider.setValue(value)
+
+    def text(self):
+        return self.label.text()
+
+    def setRange(self, minValue, maxValue):
+        if minValue < -24 or maxValue > 24 or minValue > maxValue:
+            qWarning("LCDRange::setRange(%d, %d)\n"
+                    "\tRange must be 0..99\n"
+                    "\tand minValue must not be greater than maxValue" % (minValue, maxValue))
+            return
+
+        self.slider.setRange(minValue, maxValue)
+
+    def setText(self, text):
+        self.label.setText(text)
+
+        #############
 
 #
 qt_app = QApplication(sys.argv)
 label = QLabel(G-Tycoon)
 label.show()
 qt_app.excec_()
-
